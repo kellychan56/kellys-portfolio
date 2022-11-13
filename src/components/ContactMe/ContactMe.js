044ef89b52d82174d5d40528b3377f3fb832d094
kellychan56 <kelly.chan568@gmail.com>
2022-11-12 17:54:33 -0800

    linter

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -7,8 +7,8 @@ const ContactMe = () => {
         <section id="contact">
             <h2 id="contact-me">contact me</h2>
             <a className='contact-links-anchor' href="mailto:kelly.chan568@gmail.com"><h2 className="contact-links">kelly.chan568@gmail.com</h2></a>
-            <a className='contact-links-anchor' href="https://github.com/kellychan56" target="_blank"><h2 className="contact-links">github</h2></a>
-            <a className='contact-links-anchor' href="www.linkedin.com/in/kelly-chan-574631217" target="_blank"><h2 className="contact-links">linkedin</h2></a>
+            <a className='contact-links-anchor' href="https://github.com/kellychan56" target="_blank" rel="noreferrer"><h2 className="contact-links">github</h2></a>
+            <a className='contact-links-anchor' href="www.linkedin.com/in/kelly-chan-574631217" target="_blank" rel="noreferrer"><h2 className="contact-links">linkedin</h2></a>
         </section>
     </>
       )
